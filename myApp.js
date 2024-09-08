a489acacf411e7a4952c1105561627ba594c4111
Aman <ak222760@gmail.com>
2024-09-08 10:27:59 +0530

    Meet the node console

--- myApp.js
+++ myApp.js
@@ -2,38 +2,7 @@ let express = require('express');
 let app = express();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Meet the node console
+console.log("Hello World")
 
  module.exports = app;
